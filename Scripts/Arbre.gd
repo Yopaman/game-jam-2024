e214a274f52e99b58e50f9c128f8ce8518b779a0
rboudrouss <r.boudrouss@gmail.com>
2024-05-18 03:22:27 +0200

    fixed arbre

--- Scripts/Arbre.gd
+++ Scripts/Arbre.gd
@@ -9,7 +9,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if triggered:
-		$Area3D.position.x -= 0.1
+		$ObstacleArea.position.x -= 0.1
 
 
 
