1a8f7f820eeb1468fbbbff7e6df48540077a2444
rboudrouss <r.boudrouss@gmail.com>
2024-05-18 17:59:03 +0200

    daizjdioazkdza

--- Scripts/Ours.gd
+++ Scripts/Ours.gd
@@ -3,11 +3,11 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var nbr := randi() % 4
-	if nbr > 2:
+	var nbr := randi() % 20
+	if nbr < 2:
 		$AnimatedSprite3D.hide()
 		$ours_bleu.show()
 		$ours_bleu.play()
-	
+
 func attack():
 	$AnimatedSprite3D.play("attack")
